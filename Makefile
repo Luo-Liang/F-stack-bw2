2340fcf23b462a14c91d5a6e2e1d9eefcf03f832
Ubuntu <ubuntu@ip-172-31-13-4.us-west-2.compute.internal>
2018-06-29 21:20:52 +0000

    Makefile to link both f-stack and ans, fix extern C calls

--- Makefile
+++ Makefile
@@ -1,4 +1,14 @@
-TOPDIR=..
+ifeq ($(RTE_SDK),)
+$(error "Please define RTE_SDK environment variable")
+endif
+
+ifeq ($(RTE_ANS),)
+$(error "Please define RTE_ANS environment variable")
+endif
+
+ifeq ($(RTE_TARGET),)
+$(error "Please define RTE_TARGET environment variable")
+endif
 
 ifeq ($(FF_PATH),)
 	FF_PATH=${TOPDIR}
@@ -8,16 +18,30 @@ ifeq ($(FF_DPDK),)
 	FF_DPDK=${TOPDIR}/dpdk/x86_64-native-linuxapp-gcc
 endif
 
-LIBS+= -L${FF_PATH}/lib -Wl,--whole-archive,-lfstack,--no-whole-archive
-LIBS+= -L${FF_DPDK}/lib -Wl,--whole-archive,-ldpdk,--no-whole-archive
-LIBS+= -Wl,--no-whole-archive -lrt -lm -ldl -lcrypto -pthread -lnuma
-LDFLAGS += -lstdc++
-TARGET="helloworld"
-all:
-	g++ -O3 -g -std=c++11  -gdwarf-2  -I$(FF_PATH)/lib -o ${TARGET} main.cpp ${LIBS} ${LDFLAGS}
-	g++ -O3 -g -std=c++11  -gdwarf-2  -I$(FF_PATH)/lib -o ${TARGET}_epoll main_epoll.cpp ${LIBS} ${LDFLAGS}
-	g++ -O3 -g -std=c++11  -gdwarf-2  -I$(FF_PATH)/lib -o ${TARGET}_client main_client.cpp ${LIBS} ${LDFLAGS}
 
-.PHONY: clean
+CC = gcc
+RM = rm -f
+
+CPPFLAGS += -O3 -std=c++11 \
+          -I$(RTE_ANS)/librte_ans/include \
+          -I$(RTE_ANS)/librte_anssock/include \
+	  -I$(FF_PATH)/lib
+
+LDLIBS += $(RTE_ANS)/librte_anssock/librte_anssock.a \
+          -L$(RTE_SDK)/$(RTE_TARGET)/lib \
+          -Wl,--whole-archive -Wl,-lrte_mbuf -Wl,-lrte_mempool_ring -Wl,-lrte_mempool -Wl,-lrte_ring -Wl,-lrte_eal \
+	  -Wl,-lrte_ethdev -Wl,-lrte_timer \
+	  -Wl,--no-whole-archive -Wl,-export-dynamic -lrt -pthread -ldl -lnuma -lstdc++ \
+	  -L${FF_PATH}/lib -Wl,--whole-archive,-lfstack,--no-whole-archive \
+	  -Wl,--no-whole-archive -lm  -lcrypto 
+
+OBJS = main_epoll.o main_client.o
+TARGET = main_epoll main_client
+all:$(OBJS)
+	$(CC) -o main_epoll main_epoll.o $(CPPFLAGS) $(LDLIBS)
+	$(CC) -o main_client main_client.o $(CPPFLAGS) $(LDLIBS)
+
+$(OBJS):%.o:%.cpp
+	$(CC) -c $(CPPFLAGS) $< -o $@
 clean:
-	rm -f *.o ${TARGET} ${TARGET}_epoll ${TARGET}_client
+	-$(RM) $(TARGET) $(OBJS)
--- PLinkNixEpollEAL.h
+++ PLinkNixEpollEAL.h
@@ -1,15 +1,50 @@
+#pragma once
 #include "ff_config.h"
 #include "ff_api.h"
 #include "ff_epoll.h"
+extern "C"
+{
+#include "anssock_intf.h"
+#include "ans_errno.h"
+}
+
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+#include <sys/times.h>
+
+#include <stdint.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <termios.h>
+#include <sys/epoll.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#ifndef __linux__
+#ifdef __FreeBSD__
+#include <sys/socket.h>
+#else
+#include <net/socket.h>
+#endif
+#endif
+
+#include <sys/time.h>
 
 enum PLinkEpollAPI
 {
     UseLinux,
+    UseANS,
     UseFStack
 };
 
 extern PLinkEpollAPI apiSwitch;
+extern "C" int anssock_socket(int domain, int type, int protocol);
 
 int PLinkSocket(int domain, int type, int protocol)
 {
@@ -17,6 +52,8 @@ int PLinkSocket(int domain, int type, int protocol)
     {
     case UseLinux:
         return socket(domain, type, protocol);
+    case UseANS:
+        return anssock_socket(domain, type, protocol);
     case UseFStack:
     default:
         return ff_socket(domain, type, protocol);
@@ -29,6 +66,8 @@ int PLinkEpollCtrl(int epfd, int op, int fd, epoll_event *event)
     {
     case UseLinux:
         return epoll_ctl(epfd, op, fd, event);
+    case UseANS:
+        return anssock_epoll_ctl(epfd, op, fd, event);
     case UseFStack:
     default:
         return ff_epoll_ctl(epfd, op, fd, event);
@@ -41,6 +80,8 @@ int PLinkClose(int fd)
     {
     case UseLinux:
         return close(fd);
+    case UseANS:
+        return anssock_close(fd);
     case UseFStack:
     default:
         return ff_close(fd);
@@ -53,7 +94,8 @@ ssize_t PLinkWrite(int fd, const void *buf, size_t count)
     {
     case UseLinux:
         return write(fd, buf, count);
-        break;
+    case UseANS:
+        return anssock_write(fd, buf, count);
     case UseFStack:
     default:
         return ff_write(fd, buf, count);
@@ -66,6 +108,8 @@ int PLinkInit(size_t argc, char *argv[])
     {
     case UseLinux:
         return 0; //write(fd, buf, count);
+    case UseANS:
+        return anssock_init(NULL);
     case UseFStack:
     default:
         return ff_init(argc, argv);
@@ -80,6 +124,8 @@ int PLinkSetNonBlock(int fd)
     {
     case UseLinux:
         return ioctl(fd, FIONBIO, &on);
+    case UseANS:
+        return 0;
     case UseFStack:
     default:
         return ff_ioctl(fd, FIONBIO, &on);
@@ -92,6 +138,8 @@ int PLinkConnect(int sockfd, const sockaddr *addr, socklen_t addrlen)
     {
     case UseLinux:
         return connect(sockfd, addr, addrlen);
+    case UseANS:
+        return anssock_connect(sockfd, addr, addrlen);
     case UseFStack:
     default:
         return ff_connect(sockfd, (const linux_sockaddr *)addr, addrlen);
@@ -103,6 +151,7 @@ void PLinkRun(loop_func_t loop, void *arg)
     switch (apiSwitch)
     {
     case UseLinux:
+    case UseANS:
         while (true)
         {
             loop(arg);
@@ -121,6 +170,8 @@ int PLinkAccept(int sockfd, struct sockaddr *addr, socklen_t *addrlen)
     {
     case UseLinux:
         return accept(sockfd, addr, addrlen);
+    case UseANS:
+        return anssock_accept(sockfd, addr, addrlen);
     case UseFStack:
     default:
         return ff_accept(sockfd, (linux_sockaddr *)addr, addrlen);
@@ -133,6 +184,8 @@ int PLinkListen(int sockfd, int backlog)
     {
     case UseLinux:
         return listen(sockfd, backlog);
+    case UseANS:
+        return anssock_listen(sockfd, backlog);
     case UseFStack:
     default:
         return ff_listen(sockfd, backlog);
@@ -145,6 +198,8 @@ int PLinkEpollCreate()
     {
     case UseLinux:
         return epoll_create(1);
+    case UseANS:
+        return anssock_epoll_create(1);
     case UseFStack:
     default:
         return ff_epoll_create(1);
@@ -157,6 +212,8 @@ int PLinkBind(int sockfd, const sockaddr *addr, socklen_t addrlen)
     {
     case UseLinux:
         return bind(sockfd, addr, addrlen);
+    case UseANS:
+        return anssock_bind(sockfd, addr, addrlen);
     case UseFStack:
     default:
         return ff_bind(sockfd, (linux_sockaddr *)addr, addrlen);
@@ -169,6 +226,8 @@ ssize_t PLinkRead(int fd, void *buf, size_t count)
     {
     case UseLinux:
         return read(fd, buf, count);
+    case UseANS:
+        return anssock_read(fd, buf, count);
     case UseFStack:
     default:
         return ff_read(fd, buf, count);
@@ -181,6 +240,8 @@ int PLinkEpollWait(int epfd, epoll_event *events, int maxevents, int timeout)
     {
     case UseLinux:
         return epoll_wait(epfd, events, maxevents, timeout);
+    case UseANS:
+        return anssock_epoll_wait(epfd, events, maxevents, timeout);
     case UseFStack:
         return ff_epoll_wait(epfd, events, maxevents, timeout);
     }
